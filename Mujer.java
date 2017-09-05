704f7af764c287d5a16c1a488b35bd51e87a0c00
dahlquistA <31600136+dahlquistA@users.noreply.github.com>
2017-09-05 14:38:43 -0300

    Add files via upload

---
+++ Mujer.java
@@ -0,0 +1,144 @@
+
+/**
+ * Write a description of class Mujer here.
+ * 
+ * @author Analuz Daiana Dahlquist Arana
+ * @version 1.0
+ */
+public class Mujer
+{
+    private String nombre ;
+    private String apellido;
+    private int edad;
+    private String estadoCivil;
+    private Hombre esposo;
+    
+    public Mujer (String p_nombre, String p_apellido, int p_edad)
+    {
+        this.setNombre(p_nombre);
+        this.setApellido(p_apellido);
+        this.setEdad(p_edad);
+        this.setEstadoCivil("Soltera");       
+    }
+    
+    public Mujer (String p_nombre, String p_apellido, int p_edad, Hombre p_esposo)
+    {
+        this.setNombre(p_nombre);
+        this.setApellido(p_apellido);
+        this.setEdad(p_edad);
+        this.setEstadoCivil("Casada");
+        this.setEsposo(p_esposo);
+    }
+    
+    // Inicio de los Setters:
+    private void setNombre(String p_nombre)
+    {
+        this.nombre = p_nombre;
+    }
+    
+    private void setApellido(String p_apellido)
+    {
+        this.apellido = p_apellido;
+    }
+    
+    private void setEdad(int p_edad)
+    {
+        this.edad = p_edad;
+    }
+    
+    /**
+     * Que hago acá si no recibe parámetros?
+     *
+     */
+    private void setEstadoCivil(String p_estado)
+    {
+        this.estadoCivil = p_estado;
+    }
+    
+    private void setEsposo(Hombre p_esposo)
+    {
+        this.esposo = p_esposo;
+    }
+    //Fin de los Setters
+    
+    // Inicio de los Getters:
+    public String getNombre()
+    {
+        return this.nombre;
+    }
+    
+    public String getApellido()
+    {
+        return this.apellido;
+    }
+    
+    public int getEdad()
+    {
+        return this.edad;
+    }
+    
+    public String getEstadoCivil()
+    {
+        return this.estadoCivil;
+    }
+    
+    public Hombre getEsposo()
+    {
+        return this.esposo;
+    }
+    // Fin de los Getters.
+    
+    // Inicio de los demás métodos:
+    public void casarseCon(Hombre p_esposo)
+    {
+        if (this.getEsposo() == null)
+        {
+            this.setEstadoCivil("Casada"); 
+            this.setEsposo(p_esposo);
+            this.getEsposo().casarseCon(this);
+                         
+       }
+    }
+    
+    public void divorcio()
+    {
+        /*if (this.getEstadoCivil().equals("Casada"))
+        {
+            this.setEstadoCivil("Divorciada");  
+            this.getEsposo().divorcio();                    
+            this.setEsposo(null);
+        }*/
+        System.out.println("ESPOSO: " +this.getEsposo());
+        if (this.getEsposo().getEsposa() != null)
+            
+        
+        {  
+            this.setEstadoCivil("Divorciada");
+            
+            this.getEsposo().divorcio(); 
+            this.setEsposo( null );
+            
+        } 
+        
+    }
+    
+    public String datos()
+    {
+        return (this.getNombre()+" "+this.getApellido()+" de "+
+                this.getEdad()+" años");
+    }
+    
+    public void mostrarEstadoCivil()
+    {
+        System.out.println(this.getNombre()+" "+this.getApellido()+" de "+
+                           this.getEdad()+" años - "+this.getEstadoCivil());
+    }
+    
+    public void casadaCon()
+    {
+        System.out.println(this.getNombre()+" "+this.getApellido()+" de "+
+                           this.getEdad()+" años está casada con "+
+                           this.getEsposo().datos());
+    }
+    
+} //Fin de la clase Mujer.
